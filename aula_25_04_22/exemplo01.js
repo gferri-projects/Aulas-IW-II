7362aae1603c398369f2f305e23e540e5bee0434
Julia Granado <79951728+gferri-projects@users.noreply.github.com>
2022-04-25 09:07:18 -0300

    Aula - 25/04

---
+++ aula_25_04_22/exemplo01.js
@@ -0,0 +1,3 @@
+// testando plugin 
+console.log('Testando plugin')
+// iniciar plugin tecla f8 
---
+++ aula_25_04_22/exemplo02.js
@@ -0,0 +1,12 @@
+var velocidade = 59
+
+console.log(`A velocidade do carro é de ${velocidade} km/h`)
+
+if(velocidade >=60){
+    console.log(`Você ultrapassou o limite de velocidade!`)
+    console.log(`Será MULTADO!`)
+}
+
+console.log(`Dirija com segurança!`)
+
+// NODE SÓ EXIBE COM 'CONSOLE.LOG'

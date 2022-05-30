814385eb73a54b0b9b3a227974a62637fe6b4fdf
Julia Granado <79951728+gferri-projects@users.noreply.github.com>
2022-05-30 09:10:03 -0300

    Desafio JS

---
+++ aula_30_05_22/calcula.js
@@ -0,0 +1,14 @@
+var nascimento = document.querySelector('ano')
+//var masculino = document.getElementById('masc')
+//var feminino = document.getElementById('fem')
+var data = 2022 - nascimento
+var sexo = [document.querySelector('masc'), document.querySelector('fem')]
+var botao = document.getElementById('button')
+var foto = document.getElementById('img')
+//var text = document.getElementById('texto')
+
+
+botao.onclick = function(){
+    //alert('funfo')
+      
+01,-}
